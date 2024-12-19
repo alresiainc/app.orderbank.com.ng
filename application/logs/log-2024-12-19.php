ee85b61acdf6dc59ebef6f441f714c8dfd450822
fidelis-telentsapartments <fidelisepeter@talentsapartments.com>
2024-12-19 10:54:57 +0100

    update

--- application/logs/log-2024-12-19.php
+++
@@ -1,7 +0,0 @@
-ERROR - 2024-12-19 09:47:45 --> 404 Page Not Found: Theme/plugins
-ERROR - 2024-12-19 10:47:46 --> Severity: Warning --> Array to string conversion /Applications/AMPPS/www/backend.myproda.test/application/controllers/Orders.php 341
-ERROR - 2024-12-19 09:48:56 --> 404 Page Not Found: Theme/plugins
-ERROR - 2024-12-19 10:48:57 --> Severity: Warning --> Array to string conversion /Applications/AMPPS/www/backend.myproda.test/application/controllers/Orders.php 341
-ERROR - 2024-12-19 10:50:29 --> Severity: Warning --> Array to string conversion /Applications/AMPPS/www/backend.myproda.test/application/controllers/Orders.php 341
-ERROR - 2024-12-19 10:50:50 --> Severity: Warning --> Array to string conversion /Applications/AMPPS/www/backend.myproda.test/application/controllers/Orders.php 341
-ERROR - 2024-12-19 10:52:17 --> Severity: Warning --> Array to string conversion /Applications/AMPPS/www/backend.myproda.test/application/controllers/Orders.php 341
