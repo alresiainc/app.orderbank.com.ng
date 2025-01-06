4d6b867d0dc009211a8cfbe7eb9379b19490d9de
fidelis-telentsapartments <fidelisepeter@talentsapartments.com>
2025-01-06 14:31:11 +0100

    update

--- application/logs/log-2025-01-06.php
+++ application/logs/log-2025-01-06.php
@@ -2,3 +2,4 @@
 
 ERROR - 2025-01-06 14:25:25 --> Severity: error --> Exception: Column 'status' in where clause is ambiguous /Applications/AMPPS/www/backend.myproda.test/system/database/drivers/mysqli/mysqli_driver.php 305
 ERROR - 2025-01-06 14:26:28 --> Severity: error --> Exception: Unknown column 's.state' in 'field list' /Applications/AMPPS/www/backend.myproda.test/system/database/drivers/mysqli/mysqli_driver.php 305
+ERROR - 2025-01-06 14:30:13 --> Requested type: whatsapp
